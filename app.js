0d87a9af25d5e5d71d73b48b73ef9fc0e78e38b8
Lailah Berry <lailahnicole614@gmail.com>
2023-01-18 16:48:07 -0800

    added functions for mice and cat

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const chasedCountEl = document.getElementById('chased-count');
 
 /* State */
 let chasedCount = 0;
-let cat = 9;
+let catLives = 9;
 const mice = [
     {
         name: 'speedy',
@@ -28,21 +28,67 @@ const mice = [
 
 /* Events */
 buttonEl.addEventListener('click', () => {
-    const miceName = inputEl.ariaValueMax;
+    const miceName = inputEl.value;
     if (!miceName) {
         return;
     }
-
+    //New mouse =
     const newMice = {
         name: miceName,
-        lives: Math.ceil(Math.random() * 6),
+        hp: Math.ceil(Math.random() * 6),
     };
-
+    //Moving mouse into state:
     mice.push(newMice);
     inputEl.value = '';
     displayMice();
 });
 
 /* Display Functions */
+function displayMice() {
+    miceEl.textContent = '';
+    //looping for mice in state=
+    for (let mouse of mice) {
+        const newMiceEl = renderMice(mouse);
+        // event listener
+        newMiceEl.addEventListener('click', () => {
+            if (catLives <= 0) {
+                alert('you are too tired to chase!');
+                return;
+            }
+
+            // player chases the mice with their cat:
+            if (Math.random() > 0.3) {
+                alert('you chased' + mice.name);
+                mice.hp--;
+
+                if (mice.hp === 0) {
+                    chasedCount++;
+                    chasedCountEl.textContent = `You've chased ${chasedCount} mice;`;
+                }
+            } else {
+                alert(mice.name + 'outran you!');
+            }
+
+            if (Math.random() > 0.8) {
+                alert(mice.name + 'outran you and now you are out of breath!');
+                catLives--;
+
+                if (catLives <= 0) {
+                    catImgEl.classList.add('tired');
+                }
+            } else {
+                alert(miceName + 'you caught the mouse!');
+            }
+
+            catLivesEl.textContent = catLives;
+
+            displayMice();
+        });
+
+        miceEl.append(newMiceEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+
+displayMice();
